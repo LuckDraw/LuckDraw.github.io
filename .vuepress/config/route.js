54cc69e856db5dc2ad81bf08975e84b8a6bd44d0
buuing <17778888138@189.cn>
2020-11-10 16:14:02 +0800

    docs: 更新文档

--- .vuepress/config/route.js
+++ .vuepress/config/route.js
@@ -2,10 +2,13 @@ module.exports = {
   matchRoute: {
     '/vue-luck-draw/lucky-demo': '/demo/wheel/ymc',
     '/vue-luck-draw/lucky-demo/': '/demo/wheel/ymc',
+    '/vue-luck-draw/lucky-demo.html': '/demo/wheel/ymc',
     '/vue-luck-draw/lucky-wheel': '/document/wheel/blocks',
     '/vue-luck-draw/lucky-wheel/': '/document/wheel/blocks',
+    '/vue-luck-draw/lucky-wheel.html': '/document/wheel/blocks',
     '/vue-luck-draw/lucky-grid': '/document/grid/blocks',
     '/vue-luck-draw/lucky-grid/': '/document/grid/blocks',
+    '/vue-luck-draw/lucky-grid.html': '/document/grid/blocks',
     '/vue-luck-draw': '/document/vue',
     '/vue-luck-draw/': '/document/vue',
     '/demo': '/demo/wheel/ymc',
