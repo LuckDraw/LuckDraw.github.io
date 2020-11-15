679cd006b3b2d4379def6d3e52c90667a522df0b
buuing <17778888138@189.cn>
2020-11-15 16:06:59 +0800

    docs: 更新首页

--- .vuepress/components/HomePage.vue
+++ .vuepress/components/HomePage.vue
@@ -2,61 +2,53 @@
   <article class="home-page-wrapper">
     <section class="main">
       <div>
-        <img class="logo" :src="$withBase('/logo.png')" alt="logo" />
-        <h1>幸运抽奖插件（大转盘 / 九宫格）</h1>
-        <div class="desc" style="margin: -1rem">
-          <Content/>
+        <img class="logo" :src="$withBase('/web.svg')" alt="logo" style="max-width: 18rem; height: 30%" />
+        <h1 style="margin: 1.5rem 0">基于原生 Js / Vue 的抽奖插件【大转盘 & 九宫格】</h1>
+        <div class="plugin">
+          <div>
+            <img :src="$withBase('/start.svg')" alt="frameworks" width="70%" >
+            <h1>自由化</h1>
+            <p>奖品 / 文字 / 图片 / 颜色 / 按钮均可自由配置；支持同步 / 异步抽奖；中奖概率前端 / 后端可控；</p>
+          </div>
+          <div>
+            <img :src="$withBase('/setup.svg')" alt="frameworks" width="70%" >
+            <h1>多端适配</h1>
+            <p>一个支持 JavaScript / Vue2.x / Vue3.x 的抽奖插件；</p>
+            <p>基于 React 版本还在开发中...</p>
+          </div>
+          <div>
+            <img :src="$withBase('/style.svg')" alt="frameworks" width="70%" >
+            <h1>响应式</h1>
+            <p>自动根据设备 dpr 调整清晰度；无需担心 canvas 出现绘制模糊的问题；并支持使用百分比或 rem 属性来调整移动端布局；</p>
+          </div>
         </div>
-        <router-link class="btn-about" to="/document/vue">快速开始</router-link>
+        <router-link class="btn-about" to="/document/vue" style="margin-top: 1.5rem">快速开始</router-link>
       </div>
     </section>
     <section class="md-content-wrapper">
       <!-- <Content/> -->
     </section>
-        <section class="wish">
-      <div class="wish-inner">
-        <div class="img-wrapper">
-          <img src="../public/frameworks.svg" alt="frameworks" width="70%" >
-        </div>
-        <div class="text-wrapper">
-          <h1>🎉支持多端框架</h1>
-          <li class="description">目前支持原生 Js / Vue2.x / Vue3.x 中使用，后续还会新增其他框架，感谢支持，您的 star 就是对我最大的鼓励</li>
-        </div>
-      </div>
-    </section>
     <section class="wish">
       <div class="wish-inner">
-        <div class="text-wrapper">
-          <h1>🎨可适配移动端</h1>
-          <li class="description">抽奖插件会自动根据设备 dpr 调整清晰度，无需担心 canvas 出现绘制模糊的问题</li>
-          <li class="description">并支持使用百分比或 rem 属性来调整移动端布局</li>
-        </div>
-        <div class="img-wrapper">
-          <img src="../public/devices.svg" alt="devices" width="80%" >
-        </div>
-      </div>
-    </section>
-    <section class="wish">
-      <div class="wish-inner">
-        <div class="img-wrapper">
-          <img src="../public/developer.svg" alt="developer" width="90%" >
+        <div class="img-wrapper" style="max-width: 34rem;">
+          <img src="../public/github.svg" alt="developer" width="100%" >
         </div>
         <div class="text-wrapper">
-          <h1>🚀问题与建议</h1>
-          <li class="description">欢迎在 Github 上面的 Issues 提出你的疑惑，我会实时收到提醒，并在不忙的时候第一时间回复你</li>
-        </div>
-      </div>
-    </section>
-    <section class="wish">
-      <div class="wish-inner">
-        <div class="text-wrapper">
-          <h1>🏅欢迎参与开发</h1>
+          <h1>加入我们</h1>
           <li class="description"><b>贡献源代码：</b>目前插件基于 Ts 开发然后打包成 Js，并以此为基础在不同框架上面进行扩展，如果你有熟悉的框架想进行扩展，请在底部寻找我的联系方式</li>
           <li class="description"><b>翻译文档：</b>目前也急需英语较好的同学来参与翻译文档的任务，我会在 README 里面展示所有的贡献者</li>
           <li class="description"><b>贡献Demo：</b>如果你们公司的抽奖设计图比较好看，也可以提供给我，我会放到演示页面里进行展示</li>
         </div>
+      </div>
+    </section>
+    <section class="wish">
+      <div class="wish-inner">
+        <div class="text-wrapper">
+          <h1>问题 & 建议</h1>
+          <li class="description">欢迎在 <a href="https://github.com/luckdraw" target="_black">Github</a> 上面的 Issues 提出你的疑惑，我会实时收到提醒，并在不忙的时候第一时间回复你</li>
+        </div>
         <div class="img-wrapper">
-          <img src="../public/code.svg" alt="devices" width="100%" >
+          <img src="../public/issues.svg" alt="devices" width="100%" >
         </div>
       </div>
     </section>
@@ -75,10 +67,10 @@
           <i class="iconfont reco-wechat"></i>
           new_ldq
         </span>
-        <span>
+        <!-- <span>
           <i class="iconfont reco-faq"></i>
           <a href="https://github.com/buuing/vue-luck-draw/issues">问题&建议</a>
-        </span>
+        </span> -->
         <span>
           <i class="iconfont reco-copyright"></i>
           2019
@@ -122,7 +114,7 @@ export default {
   max-width: 12rem;
   width: 100%;
   display: block;
-  margin: 0rem auto 1rem;
+  margin: 1rem auto 1rem;
 }
 .home-page-wrapper section.main {
   display: flex;
@@ -209,6 +201,31 @@ export default {
   box-sizing: border-box;
   padding: 0 2rem;
 }
+.home-page-wrapper .plugin {
+  max-width: 56rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.home-page-wrapper .plugin>div {
+  padding: 1.5rem 0 1rem;
+  width: 30%;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+  background: #fff;
+}
+.home-page-wrapper .plugin>div h1 {
+  font-size: 18px;
+  color: #1f2f3d;
+  font-weight: 400;
+}
+.home-page-wrapper .plugin>div p {
+  font-size: 14px;
+  color: #99a9bf;
+  padding: 0 25px;
+  line-height: 20px;
+  margin: 1rem 0;
+}
 @media (max-width: 719px) {
   .home-page-wrapper section {
     padding: 0 2rem;
@@ -234,6 +251,19 @@ export default {
   }
   .home-page-wrapper section.wish .wish-inner .img-wrapper {
     margin: 0 auto;
+  }
+  .home-page-wrapper section.wish .wish-inner {
+    display: block;
+    padding: 2rem 0;
+  }
+  .home-page-wrapper section.wish .wish-inner .img-wrapper {
+    margin: 0 auto;
+  }
+  .home-page-wrapper .plugin>div {
+    width: 45%;
+  }
+  .home-page-wrapper .plugin>div:nth-child(n+3) {
+    display: none;
   }
 }
 @media (max-width: 419px) {
@@ -261,6 +291,19 @@ export default {
   }
   .home-page-wrapper section.wish .wish-inner .img-wrapper {
     margin: 0 auto;
+  }
+  .home-page-wrapper section.wish .wish-inner {
+    display: block;
+    padding: 2rem 0;
+  }
+  .home-page-wrapper section.wish .wish-inner .img-wrapper {
+    margin: 0 auto;
+  }
+  .home-page-wrapper .plugin>div {
+    width: 100%;
+  }
+  .home-page-wrapper .plugin>div:nth-child(n+2) {
+    display: none;
   }
 }
 </style>
